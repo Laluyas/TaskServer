ff6ed24378dcdad7e6d52acb169a36690b2b5b01
Yazeed <laluyazeed00@gmail.com>
2024-07-15 02:01:59 +0530

    asd

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 })
 .then(() => {
-    app.listen(4000, ()=>{
-        console.log("Server listening on port 4000")
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server listening on port ${process.env.PORT}`)
     })
 })
 .catch((error) => {
